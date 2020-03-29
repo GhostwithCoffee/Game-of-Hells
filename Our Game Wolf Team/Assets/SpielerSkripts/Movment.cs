e40295b946d809a960293690352b4835dea26bb5
DragonflyPlay11 <62449134+DragonflyPlay11@users.noreply.github.com>
2020-03-29 21:42:29 +0200

    Created Hub Area, added Playermovment

---
+++ Our Game Wolf Team/Assets/SpielerSkripts/Movment.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movment : MonoBehaviour
+{
+    public float speed = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovmentPlayer();
+    }
+
+    // Movment method for the player
+    void MovmentPlayer()
+    {
+        Vector3 newPos = transform.position;
+        if (Input.GetKey("w"))
+        {
+            newPos.y += speed * Time.deltaTime;
+            Debug.Log("W ingeputtet");
+        }
+        if (Input.GetKey("s"))
+        {
+            newPos.y -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            newPos.x -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            newPos.x += speed * Time.deltaTime;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, newPos, speed * Time.deltaTime); ;
+    }
+
+}
